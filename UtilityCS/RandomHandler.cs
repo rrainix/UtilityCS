30946bcfa1a1e4f17f9a29b9aff2dca1024188bb
Beluartlauinc <73387929+Beluartlauinc@users.noreply.github.com>
2025-09-19 22:39:40 +0200

    Add RandomHandler.cs

---
+++ UtilityCS/RandomHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilityCS
+{
+    public static class RandomHandler
+    {
+        private static Random random = new Random();
+
+        public static int Range(int min, int max) => random.Next(min, max);
+        public static double Range(double min, double max) => random.Next(min, max);
+        public static bool FlipCoint() => random.FlipCoin();
+        public static bool Chance(float threshold, float max = 1.0f) => random.Chance(threshold, max);
+    }
+}
